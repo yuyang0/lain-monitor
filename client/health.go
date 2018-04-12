950b54deacdf4c3c86c09a56a15b6e7b82c12806
YuYang <yyangplus@gmail.com>
2018-04-12 11:41:18 +0800

    add lvault health check

--- client/health.go
+++ client/health.go
@@ -14,7 +14,7 @@ import (
 )
 
 const (
-	TIMEOUT              = 5
+	TIMEOUT              = 2
 	HEALTHCHECK_INTERVAL = 60
 )
 
@@ -36,10 +36,16 @@ var (
 
 	tinydnsMetric = "lain.tinydns.health"
 	tinydnsURL    = fmt.Sprintf("http://%s/ping", serverAddr)
+
+	lvaultURL    = "http://lvault.lain.local/v2/status"
+	lvaultMetric = "lain.lvault.health"
+
+	vaultURL    = "http://lvault.lain.local/v2/vaultstatus"
+	vaultMetric = "lain.vault.health"
 )
 
 type HealthChecker interface {
-	Check(logger *zap.Logger) *backend.Metric
+	Check(logger *zap.Logger) []*backend.Metric
 }
 
 type urlHealthChecker struct {
@@ -58,18 +64,40 @@ type consulHealthChecker struct {
 	client     *api.Client
 }
 
-func _buildPacket(name string, isAlive bool) *backend.Metric {
-	health := 1
-	if isAlive == false {
-		health = 0
-	}
+type lvaultHealthChecker struct {
+	metricName, url string
+}
+
+type vaultHealthChecker struct {
+	metricName, url string
+}
+
+func _buildPacket(name string, aliveInstance int) *backend.Metric {
 	return &backend.Metric{
 		Path:      name,
-		Value:     float64(health),
+		Value:     float64(aliveInstance),
 		Tags:      map[string]string{"cluster": cfg.ClusterName},
 		Timestamp: time.Now(),
 		Step:      HEALTHCHECK_INTERVAL,
 	}
+}
+
+func _getJSON(url string, v interface{}) (bool, error) {
+	timeout := TIMEOUT * time.Second
+	client := http.Client{
+		Timeout: timeout,
+	}
+	resp, err := client.Get(url)
+	isAlive := true
+	if err != nil || resp.StatusCode != 200 {
+		isAlive = false
+		return isAlive, err
+	}
+	if err := json.NewDecoder(resp.Body).Decode(v); err != nil {
+		isAlive = false
+		return isAlive, err
+	}
+	return true, nil
 }
 
 func newConsulHealthChecker(metric, addr string) (HealthChecker, error) {
@@ -89,49 +117,110 @@ func newConsulHealthChecker(metric, addr string) (HealthChecker, error) {
 	}, nil
 }
 
-func (ckr *urlHealthChecker) Check(logger *zap.Logger) *backend.Metric {
+func (ckr *urlHealthChecker) Check(logger *zap.Logger) []*backend.Metric {
 	timeout := TIMEOUT * time.Second
 	client := http.Client{
 		Timeout: timeout,
 	}
 	resp, err := client.Get(ckr.url)
-	isAlive := true
+	aliveInstances := 1
 	if err != nil || resp.StatusCode != 200 {
-		isAlive = false
+		aliveInstances = 0
 	}
-	return _buildPacket(ckr.metricName, isAlive)
+	m := _buildPacket(ckr.metricName, aliveInstances)
+	return []*backend.Metric{m}
 }
 
-func (ckr *etcdHealthChecker) Check(logger *zap.Logger) *backend.Metric {
-	timeout := TIMEOUT * time.Second
-	client := http.Client{
-		Timeout: timeout,
-	}
-	resp, err := client.Get(ckr.url)
-	isAlive := true
-	if err != nil || resp.StatusCode != 200 {
-		isAlive = false
-		return _buildPacket(ckr.metricName, isAlive)
-	}
+func (ckr *etcdHealthChecker) Check(logger *zap.Logger) []*backend.Metric {
 	var data map[string]string
-	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-		logger.Error("failed to unmarshal from etcd resp.", zap.Error(err))
-		isAlive = false
-		return _buildPacket(ckr.metricName, isAlive)
+	aliveInstances := 1
+	ok, err := _getJSON(ckr.url, &data)
+	if !ok || err != nil {
+		if err != nil {
+			logger.Error("failed to get json from etcd", zap.Error(err))
+		}
+		aliveInstances = 0
 	}
+
 	if health, ok := data["health"]; !ok || health == "false" {
-		isAlive = false
+		aliveInstances = 0
 	}
-	return _buildPacket(ckr.metricName, isAlive)
+
+	m := _buildPacket(ckr.metricName, aliveInstances)
+	return []*backend.Metric{m}
 }
 
-func (ckr *consulHealthChecker) Check(logger *zap.Logger) *backend.Metric {
+func (ckr *consulHealthChecker) Check(logger *zap.Logger) []*backend.Metric {
 	leader, err := ckr.client.Status().Leader()
-	isHealth := true
+	aliveInstances := 1
 	if err != nil || len(leader) == 0 {
-		isHealth = false
+		aliveInstances = 0
 	}
-	return _buildPacket(ckr.metricName, isHealth)
+	m := _buildPacket(ckr.metricName, aliveInstances)
+	return []*backend.Metric{m}
+}
+
+func (ckr *lvaultHealthChecker) Check(logger *zap.Logger) []*backend.Metric {
+	type LvaultStatus struct {
+		Host   string
+		IsMiss bool
+	}
+	var metrics []*backend.Metric
+	var statusList []LvaultStatus
+	isAlive, err := _getJSON(ckr.url, &statusList)
+	if !isAlive || err != nil {
+		if err != nil {
+			logger.Error("failed to get json for lvaut", zap.Error(err))
+		}
+		metrics = append(metrics, _buildPacket(ckr.metricName, 0))
+	}
+	aliveInstances := 0
+	for _, status := range statusList {
+		if !status.IsMiss {
+			aliveInstances++
+		}
+	}
+	metrics = append(metrics, _buildPacket(ckr.metricName, aliveInstances))
+	return metrics
+}
+
+func (ckr *vaultHealthChecker) Check(logger *zap.Logger) []*backend.Metric {
+	sealedMetric := "lain.vault.sealed"
+
+	type VaultInfo struct {
+		ContainerIP   string `json:"container_ip"`
+		ContainerPort int    `json:"container_port"`
+	}
+	type VaultStatus struct {
+		Sealed         bool
+		T, N, Progress int
+	}
+	type VaultTotalInfo struct {
+		Info   VaultInfo
+		Status VaultStatus
+	}
+	var metrics []*backend.Metric
+	var statusMap map[string]VaultTotalInfo
+	isAlive, err := _getJSON(ckr.url, &statusMap)
+	if !isAlive || err != nil {
+		if err != nil {
+			logger.Error("failed to get json for vault", zap.Error(err))
+		}
+		metrics = append(metrics, _buildPacket(ckr.metricName, 0))
+	}
+	aliveInstances := 0
+	sealedInstances := 0
+	for _, status := range statusMap {
+		sealed := status.Status.Sealed
+		if !sealed {
+			aliveInstances++
+		} else {
+			sealedInstances++
+		}
+	}
+	metrics = append(metrics, _buildPacket(ckr.metricName, aliveInstances))
+	metrics = append(metrics, _buildPacket(sealedMetric, sealedInstances))
+	return metrics
 }
 
 func runHealthCheckers(ctx context.Context, bd backend.Backend, logger *zap.Logger) error {
@@ -156,6 +245,8 @@ func runHealthCheckers(ctx context.Context, bd backend.Backend, logger *zap.Logg
 		checkers = append(checkers, &urlHealthChecker{name, url})
 	}
 	checkers = append(checkers, &etcdHealthChecker{etcdMetric, etcdURL})
+	checkers = append(checkers, &lvaultHealthChecker{lvaultMetric, lvaultURL})
+	checkers = append(checkers, &vaultHealthChecker{vaultMetric, vaultURL})
 
 	consulCkr, err := newConsulHealthChecker(consulMetric, consulAddr)
 	if err != nil {
@@ -170,7 +261,7 @@ func runHealthCheckers(ctx context.Context, bd backend.Backend, logger *zap.Logg
 			case now := <-ticker.C:
 				logger.Info("health check...", zap.Time("now", now))
 				for _, ckr := range checkers {
-					packets = append(packets, ckr.Check(logger))
+					packets = append(packets, ckr.Check(logger)...)
 				}
 				bd.Send(packets, logger)
 				logger.Info("health check done.", zap.Time("now", now))
